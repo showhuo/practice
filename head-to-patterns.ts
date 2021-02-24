8741156fa6125dd2380467ade1b2fe0e95c84c8b
onlyc <15225775+showhuo@users.noreply.github.com>
2021-02-24 23:11:35 +0800

    Add more patterns

--- head-to-patterns.ts
+++ head-to-patterns.ts
@@ -239,6 +239,7 @@ class Receiver {
 }
 
 class Invoker {
+    // 可以支持多个，比如一组按键对应不同命令
     myCommand: Command;
     setCommand(command: Command) {
         this.myCommand = command;
@@ -287,3 +288,261 @@ invoker.executeMyCommand();
 invoker.setCommand(macroCommand);
 invoker.executeMyCommand();
 invoker.undoMyCommand();
+
+
+// 适配器模式将不适配的接口们转换成一个适配 Client 的接口
+// 表面模式（Facade）简化接口，方便使用
+interface DuckType {
+    quack(): void;
+    fly(): void;
+}
+
+class Turkey {
+    goggle() {
+        console.log(`I am a turkey goggling`);
+    }
+    fly() {
+        console.log(`Turkey flying!`);
+    }
+}
+
+class TurkeyToDuckAdapter implements DuckType {
+    turKey: Turkey;
+    constructor(turkey: Turkey) {
+        this.turKey = turkey;
+    }
+    quack() {
+        this.turKey.goggle();
+    }
+    fly() {
+        this.turKey.fly();
+    }
+}
+
+class DuckA implements DuckType {
+    quack() {
+        console.log(`Duck quacking`);
+    }
+    fly() {
+        console.log(`Duck flying`);
+    }
+}
+
+class FacadeForAnimals {
+    duck: DuckType;
+    turkey: Turkey;
+    constructor(duck: DuckType, turkey: Turkey) {
+        this.duck = duck;
+        this.turkey = turkey;
+    }
+    makeNoise() {
+        this.duck.quack();
+        this.turkey.goggle();
+    }
+}
+
+
+// 模板方法模式 Template Method
+abstract class TemplateClass {
+    templateMethod() {
+        this.lifeCycleMethodA();
+        this.lifeCycleMethodB();
+        this.lifeCycleMethodC();
+        if (this.hookD()) {
+            this.hookD();
+        }
+    }
+    // 这两个必须由子类实现
+    abstract lifeCycleMethodA(): void;
+    abstract lifeCycleMethodB(): void;
+    lifeCycleMethodC() {
+        console.log(`lifeCycleMethodC is shared by all.`);
+    }
+    hookD() {
+        console.log(`hookD is a hook method, it is optional for subclasses`);
+        return null;
+    }
+}
+
+
+// 组合模式，composite 节点和 leaf 节点拥有相同接口，Client 不需要区分这两种节点
+abstract class ComponentNode {
+    add() {
+        throw new Error("No add method available");
+    }
+    rename() {
+        throw new Error("No add method available");
+    }
+    print() {
+        throw new Error("No add method available");
+    }
+}
+
+class CompositeNode extends ComponentNode {
+    add() {
+        console.log(`CompositeNode is adding something`);
+    }
+    print() {
+        console.log(`CompositeNode is printing`);
+    }
+}
+
+class LeafNode extends ComponentNode {
+    rename() {
+        console.log(`LeafNode is renaming`);
+    }
+    print() {
+        console.log(`LeafNode is printing`);
+    }
+}
+
+class ClientForCompositePattern {
+    node: ComponentNode;
+    constructor(node: ComponentNode) {
+        this.node = node;
+    }
+    addNode() {
+        this.node.add();
+    }
+    renameNode() {
+        this.node.rename();
+    }
+    printNode() {
+        this.node.print();
+    }
+}
+
+
+// State pattern 状态模式，又称为有限状态机，有点像策略模式，但针对性不同
+// 所有的状态类实现相同的 State 接口，尽管它们的 actions 名称相同，但作用不相同
+// Machine 类将 actions 调用委托给 State state，调用之后 state 可能会发生替换，它们相互影响
+abstract class StatePatternState {
+    abstract name: string;
+    insertCoin() {
+        console.error(`can not insert coin at state ${this.name}`);
+    }
+    play() {
+        console.error(`can not play at state ${this.name}`);
+    }
+    ejectCoin() {
+        console.error(`can not eject coin at state ${this.name}`);
+    }
+}
+
+class StatePatternMachine implements StatePatternState {
+    name: 'Finite state machine';
+    noCoinState: NoCoinState;
+    hasCoinState: HasCoinState;
+    isBrokenState;
+    currentState: StatePatternState;
+    coins: number = 0;
+    constructor(noCoinState: NoCoinState, hasCoinState) {
+        this.noCoinState = noCoinState;
+        this.hasCoinState = hasCoinState;
+    }
+    insertCoin() {
+        this.currentState.insertCoin();
+    }
+    play() {
+        this.currentState.play();
+    }
+    ejectCoin() {
+        this.currentState.ejectCoin();
+    }
+    setState(state: StatePatternState) {
+        this.currentState = state;
+    }
+    addCoin() {
+        this.coins++;
+    }
+    reduceCoin() {
+        if (this.coins > 0) this.coins--;
+    }
+}
+
+class NoCoinState extends StatePatternState {
+    name: 'NoCoinState';
+    machine: StatePatternMachine
+    constructor(machine: StatePatternMachine) {
+        super()
+        this.machine = machine;
+    }
+    insertCoin() {
+        this.machine.addCoin();
+        this.machine.setState(this.machine.hasCoinState);
+        console.log('Now you can play!');
+    }
+    play() {
+        console.error('Pls insert coin first!');
+    }
+}
+
+class HasCoinState extends StatePatternState {
+    name: 'HasCoinState';
+    machine: StatePatternMachine
+    constructor(machine: StatePatternMachine) {
+        super()
+        this.machine = machine;
+    }
+    insertCoin() {
+        this.machine.addCoin();
+        console.log(`Now you have ${this.machine.coins} coins, you can play!`);
+    }
+    play() {
+        this.machine.reduceCoin();
+        console.log(`Game start!`);
+    }
+    ejectCoin() {
+        // 思考题：coins 数量判断由 Machine 负责，还是 State 自己负责？
+        // 在实践中我们常说，状态应该尽可能由上层管理，下层组件应该是纯函数
+        // 又说组件应该内聚，它们是矛盾的吗？
+    }
+}
+
+
+// Proxy pattern 代理模式，就是访问控制
+// 比如：权限控制、虚拟代理、缓存代理等等
+interface ProxyPatternSubject {
+    methodNeedsAuth(): void;
+    methodBNeedsTimeToCreate(): void;
+    methodCPreferCache(): object;
+}
+
+class ProxyPatternProxy implements ProxyPatternSubject {
+    subject: ProxyPatternSubject;
+    hasAuth: boolean;
+    lazyObject: ProxyPatternSubject;
+    virtualObject: ProxyPatternSubject;
+    cacheObject: object;
+    constructor(subject: ProxyPatternSubject) {
+        this.subject = subject;
+    }
+    methodNeedsAuth() {
+        if (this.hasAuth) {
+            this.subject.methodNeedsAuth();
+        } else {
+            console.error(`${this.subject} has no permission`);
+        }
+    }
+    methodBNeedsTimeToCreate() {
+        if (this.lazyObject) {
+            // 已创建，可使用目标
+            this.lazyObject.methodBNeedsTimeToCreate();
+        } else {
+            // 创建开销较大，比如网络请求，此时先使用虚拟对象，假设5秒创建完成，那时再使用真实对象
+            // 不同于缓存代理，假设业务需要每次真实创建
+            this.virtualObject.methodBNeedsTimeToCreate();
+            setTimeout(() => {
+                this.lazyObject.methodBNeedsTimeToCreate();
+            }, 5000);
+        }
+    }
+    methodCPreferCache() {
+        if (this.cacheObject) {
+            return this.cacheObject;
+        } else {
+            // 同步创建或者异步创建都有可能，根据业务需要
+            // 如果是异步创建，无论是否有缓存，都包装成 Promise 给 Client 使用
+        }
+    }
+}
